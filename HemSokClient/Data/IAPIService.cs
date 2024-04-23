c77117996646b9fc047251550c82ae79a4d05a24
fblixt <fblixt@gmail.com>
2024-04-23 18:10:59 +0200

    push

--- HemSokClient/Data/IAPIService.cs
+++ HemSokClient/Data/IAPIService.cs
@@ -1,7 +1,18 @@
-﻿namespace HemSokClient.Data
+﻿using HemSokClient.Models;
+
+namespace HemSokClient.Data
 {
     public interface IAPIService
     {
+        public List<Agency> Agencies { get; set; }
+        public List<Agent> Agents { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<County> Counties { get; set; }
+        public List<Municipality> Municipality { get; set; }
+        public List<Residence> Residences { get; set; }
+        public HttpClient Client { get; }
+
+
         Task<bool> DeleteFromApiAsync<T>(string uri, T modelData) where T : class;
         Task<List<T>> GetAllFromApiAsync<T>() where T : class;
         Task<T> GetFromApiAsync<T>(string uri) where T : class;
