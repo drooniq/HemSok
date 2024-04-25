652377983407a03a19eb2437d4e94c566532ec85
mKarlsson14 <mackankb98@hotmail.se>
2024-04-25 12:40:52 +0200

    Added simple search components

---
+++ HemSokClient/SearchModel/SearchModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HemSokClient.SearchModel
+{
+    public class SearchModel
+    {
+        public string StreetSearch { get; set; } = "";
+        public string MunSearch { get; set; } = "";
+        public string CountySearch { get; set; } = "";
+    }
+}
