ad0471457d09d33b705911e9328ea2cc763f10d9
Drooniq <emil.waara@protonmail.com>
2024-05-15 09:20:53 +0200

    Added AuthStateService and minor other fixes.

---
+++ HemSokClient/Data/AuthStateService.cs
@@ -0,0 +1,32 @@
+﻿using HemSokClient.Models.LoginModels;
+
+/*
+ Author: Emil Waara
+ */
+namespace HemSokClient.Data
+{
+    public class AuthStateService : IAuthStateService
+    {
+        public CurrentUser? currentUser { get; set; } = null!;
+        public AuthStateService() { }
+
+        public bool IsLoggedIn() => currentUser != null;
+        public bool IsAdmin() => currentUser?.Role == Constants.UserRoles.Admin;
+        public bool IsAgent() => currentUser?.Role == Constants.UserRoles.Agent;
+        public bool IsSuperAdmin() => currentUser?.Role == Constants.UserRoles.SuperAdmin;
+        public void Login(CurrentUser user) => currentUser = user;
+        public void Logout() => currentUser = null;
+
+
+        public bool HasValidToken()
+        {
+            return currentUser?.loginResponse?.JwtToken != null &&
+                   currentUser.loginResponse.ExpirationDate >= DateTime.Now;
+        }
+
+        public string? GetToken()
+        {
+            return (HasValidToken() ? currentUser?.loginResponse?.JwtToken : null);
+        }
+    }
+}
--- HemSokClient/Models/LoginModels/CurrentUser.cs
+++ HemSokClient/Models/LoginModels/CurrentUser.cs
@@ -6,7 +6,6 @@ namespace HemSokClient.Models.LoginModels
     public class CurrentUser
     {
         public string? AgentId { get; set; }
-        //public Agent CurrentAgent { get; set; }
         public LoginResponse loginResponse { get; set; }
         public string? Role { get; set; }
     }
