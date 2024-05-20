376f7c8ac2ecfeb8896c74db4c7152590a5c52f4
fblixt <fblixt@gmail.com>
2024-05-20 12:31:19 +0200

    seed changes

--- HemSok/Helper/SeedAgent.cs
+++ HemSok/Helper/SeedAgent.cs
@@ -24,33 +24,21 @@ namespace HemSok.Helper
                 {
                     new AgentDTO
                     {
-                        Firstname = "Super",
-                        Lastname = "Admin",
+                        Firstname = "Green",
+                        Lastname = "Ghost",
                         Agency = agencies[0],
-                        Nickname = "Slimer",
-                        Email = "super@super.se",
-                        Username = "super@super.se",
+                        Nickname = "Onionhead",
+                        Email = "green.ghost@ghostbusters.se",
+                        Username = "green.ghost@ghostbusters.se",
                         Password = "Super@1",
                         Role = "SuperAdmin",
                         ImagePath = "http://www.solidimagearts.com/Digital/Digital_Images/slimer.jpg"
                     },
                     new AgentDTO
                     {
-                        Firstname = "Richard",
-                        Lastname = "Sörman",
-                        Agency = agencies[1],
-                        Nickname = "Ricardo",
-                        Email = "richard.sorman@widerlov.se",
-                        Username = "richard.sorman@widerlov.se",
-                        Password = "Ricardo@1",
-                        Role = "Admin",
-                        ImagePath = "https://bilder.hemnet.se/images/broker_profile_large/aa/50/aa5011334fddb7fc28af82f0fd9e3eb1.jpg"
-                    },
-                    new AgentDTO
-                    {
                         Firstname = "Nicholas",
                         Lastname = "de Mimsy-Porpington",
-                        Agency = agencies[3],
+                        Agency = agencies[1],
                         Nickname = "Nearly Headless Nick",
                         Email = "porpington@hogwartshemligahem.se",
                         Username = "porpington@hogwartshemligahem.se",
@@ -62,58 +50,22 @@ namespace HemSok.Helper
                     {
                         Firstname = "Myrtle",
                         Lastname = "Warren",
-                        Agency = agencies[3],
+                        Agency = agencies[1],
                         Nickname = "Moaning Myrtle",
                         Email = "warren@hogwartshemligahem.se",
                         Username = "warren@hogwartshemligahem.se",
                         Password = "Warren@1",
-                        Role = "Admin",
+                        Role = "Agent",
                         ImagePath = "https://yt3.ggpht.com/-6DzA-RYzCaA/AAAAAAAAAAI/AAAAAAAAAAA/nsLEw40RKKU/s900-c-k-no/photo.jpg"
-                    },
-                    new AgentDTO
-                    {
-                        Firstname = "Erik",
-                        Lastname = "Lundkvist",
-                        Agency = agencies[1],
-                        Nickname = "Erika",
-                        Email = "erik.lundkvist@widerlov.se",
-                        Username = "erik.lundkvist@widerlov.se",
-                        Password = "Erik@1",
-                        Role = "Agent",
-                        ImagePath = "https://bilder.hemnet.se/images/broker_profile_large/9f/5f/9f5f78fdc4a36a40edff23ef17a92e90.png"
-                    },
-                    new AgentDTO
-                    {
-                        Firstname = "Patrik",
-                        Lastname = "Lundberg",
-                        Agency = agencies[1],
-                        Nickname = "Patte",
-                        Email = "patrik.lundberg@widerlov.se",
-                        Username = "patrik.lundberg@widerlov.se",
-                        Password = "Lundberg@1",
-                        Role = "Agent",
-                        ImagePath = "https://bilder.hemnet.se/images/broker_profile_large/53/47/53472eb67e182e2eb52ba36866950231.png"
-                    },
-                    new AgentDTO
-                    {
-                        Firstname = "Helen",
-                        Lastname = "Lindström",
-                        Agency = agencies[2],
-                        Nickname = "Ellen",
-                        Email = "helen.lindstrom@lansforsakringar.se",
-                        Username = "helen.lindstrom@lansforsakringar.se",
-                        Password = "Lindström@1",
-                        Role = "Admin",
-                        ImagePath = "https://bilder.hemnet.se/images/broker_profile_large/7d/e0/7de0eceaf4b032a000cf86a9dabcb4af.jpg"
                     },
                     new AgentDTO
                     {
                         Firstname = "Sir Simon",
                         Lastname = "de Canterville",
-                        Agency = agencies[0],
+                        Agency = agencies[8],
                         Nickname = "The Canterville Ghost",
-                        Email = "simon.canterville@ghostbusters.se",
-                        Username = "simon.canterville@ghostbusters.se",
+                        Email = "simon.canterville@hauntedhaven.se",
+                        Username = "simon.canterville@hauntedhaven.se",
                         Password = "Canterville@1",
                         Role = "Admin",
                         ImagePath = "https://i.pinimg.com/originals/e9/07/e6/e907e6162817cac025d8ff7bcdfeb59e.jpg"
@@ -122,12 +74,12 @@ namespace HemSok.Helper
                     {
                         Firstname = "Tove",
                         Lastname = "Jansson",
-                        Agency = agencies[5],
+                        Agency = agencies[7],
                         Nickname = "Mårran",
-                        Email = "tove.jansson@poltergeistproperties.se",
-                        Username = "tove.jansson@poltergeistproperties.se",
+                        Email = "tove.jansson@wraithresidences.se",
+                        Username = "tove.jansson@wraithresidences.se",
                         Password = "Jansson@1",
-                        Role = "Admin",
+                        Role = "Agent",
                         ImagePath = "https://i.pinimg.com/originals/92/73/99/927399cbae443d7b95fe9d6a755d787e.jpg"
                     },
                     new AgentDTO
@@ -146,14 +98,235 @@ namespace HemSok.Helper
                     {
                         Firstname = "Casper",
                         Lastname = "Ghost",
-                        Agency = agencies[0],
+                        Agency = agencies[8],
                         Nickname = "The Friendly Ghost",
-                        Email = "casper.ghost@ghostbusters.se",
-                        Username = "casper.ghost@ghostbusters.se",
+                        Email = "casper.ghost@hauntedhaven.se",
+                        Username = "casper.ghost@hauntedhaven.se",
                         Password = "Ghost@1",
                         Phonenumber = "0730551498",
                         Role = "Agent",
                         ImagePath = "https://th.bing.com/th/id/R.cd19f1a1ffb22cb9d0a3e5b9eba9b52c?rik=bRWygfFyscygqw&riu=http%3a%2f%2fimages6.fanpop.com%2fimage%2fphotos%2f39900000%2fCasper-casper-the-ghost-39982351-808-960.jpg&ehk=pNL3c52nVoYamlAtK%2fBFbQQq%2bFCAv7BT94T0S1%2fJDpI%3d&risl=&pid=ImgRaw&r=0"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Winston",
+                        Lastname = "Zeddemore",
+                        Agency = agencies[0],
+                        Nickname = "",
+                        Email = "winston.zeddemore@ghostbusters.se",
+                        Username = "winston.zeddemore@ghostbusters.se",
+                        Password = "Zeddemore@1",
+                        Phonenumber = "0731112233",
+                        Role = "Admin",
+                        ImagePath = "https://upload.wikimedia.org/wikipedia/en/5/53/Winston_Zeddemore_%28Ernie_Hudson%29.jpg?20220124081144"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Raymond",
+                        Lastname = "Stantz",
+                        Agency = agencies[0],
+                        Nickname = "Ray",
+                        Email = "raymond.stantz@ghostbusters.se",
+                        Username = "raymond.stantz@ghostbusters.se",
+                        Password = "Stantz@1",
+                        Phonenumber = "0731112244",
+                        Role = "Agent",
+                        ImagePath = "https://th.bing.com/th/id/R.7f453069a9fa50d68404d9e832741bed?rik=bDr6SBLojkLZJg&riu=http%3a%2f%2fimages4.wikia.nocookie.net%2f__cb20101210000637%2fghostbusters%2fimages%2f9%2f90%2fStantz_01.jpg&ehk=IGPrZj3LS1VLvK4oFqxRd283Zv6cHMXEzxcWXx92RWc%3d&risl=&pid=ImgRaw&r=0"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Vlad",
+                        Lastname = "Dracula",
+                        Agency = agencies[9],
+                        Nickname = "Pålspetsaren",
+                        Email = "vlad.dracula@mysticmansions.se",
+                        Username = "vlad.dracula@mysticmansions.se",
+                        Password = "Dracula@1",
+                        Phonenumber = "0732223344",
+                        Role = "Admin",
+                        ImagePath = "https://media1.s-nbcnews.com/j/streams/2013/october/131031/8c9549782-131031-vladphoto-hmed-1015a-files.nbcnews-ux-2880-1000.jpg"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Morticia",
+                        Lastname = "Addams",
+                        Agency = agencies[9],
+                        Nickname = "Plum-lipped Cupid",
+                        Email = "morticia.addams@mysticmansions.se",
+                        Username = "morticia.addams@mysticmansions.se",
+                        Password = "Morticia@1",
+                        Phonenumber = "0732223355",
+                        Role = "Agent",
+                        ImagePath = "https://upload.wikimedia.org/wikipedia/en/2/23/Morticia_adams_origional.jpg"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Morgana",
+                        Lastname = "Talbot",
+                        Agency = agencies[2],
+                        Nickname = "Elvira: Mistress of the Dark",
+                        Email = "morgana.talbot@boorealty.se",
+                        Username = "morgana.talbot@boorealty.se",
+                        Password = "Talbot@1",
+                        Phonenumber = "0733334455",
+                        Role = "Admin",
+                        ImagePath = "https://th.bing.com/th/id/OIP.M09xZjgYErpLCV0BL59qIgHaLH?rs=1&pid=ImgDetMain"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Beetlejuice",
+                        Lastname = "Beetlejuice",
+                        Agency = agencies[2],
+                        Nickname = "BJ",
+                        Email = "beetlejuice@boorealty.se",
+                        Username = "beetlejuice@boorealty.se",
+                        Password = "Beetlejuice@1",
+                        Phonenumber = "0733334466",
+                        Role = "Agent",
+                        ImagePath = "https://th.bing.com/th/id/R.67282bab0fc0c1b6a3df4b14f8044006?rik=PnKAkFdADBaBfQ&pid=ImgRaw&r=0"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Mary I",
+                        Lastname = "Tudor",
+                        Agency = agencies[3],
+                        Nickname = "Bloody Mary",
+                        Email = "mary.tudor@poltergeistproperties.se",
+                        Username = "mary.tudor@poltergeistproperties.se",
+                        Password = "Tudor@1",
+                        Phonenumber = "0734445566",
+                        Role = "Admin",
+                        ImagePath = "https://www.historyonthenet.com/wp-content/uploads/2017/05/mary-tudor.jpg"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Lady Jane",
+                        Lastname = "Grey",
+                        Agency = agencies[3],
+                        Nickname = "Nine Days' Queen",
+                        Email = "jane.grey@poltergeistproperties.se",
+                        Username = "jane.grey@poltergeistproperties.se",
+                        Password = "Grey@1",
+                        Phonenumber = "0734445577",
+                        Role = "Agent",
+                        ImagePath = "https://images.twinkl.co.uk/tw1n/image/private/t_630/u/ux/twinkl-lady-jane-grey-1_ver_1.png"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Jason",
+                        Lastname = "Voorhees",
+                        Agency = agencies[4],
+                        Nickname = "Camp Crystal Lake Killer",
+                        Email = "jason.voorhees@skrackfastigheter.se",
+                        Username = "jason.voorhees@skrackfastigheter.se",
+                        Password = "Voorhees@1",
+                        Phonenumber = "0735556677",
+                        Role = "Admin",
+                        ImagePath = "https://morbidlybeautiful.com/wp-content/uploads/2016/06/jason-voorhees-friday-the-13th.jpg"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Robert",
+                        Lastname = "Gray",
+                        Agency = agencies[4],
+                        Nickname = "Bob",
+                        Email = "robert.Gray@skrackfastigheter.se",
+                        Username = "robert.Gray@skrackfastigheter.se",
+                        Password = "Gray@1",
+                        Phonenumber = "0735556688",
+                        Role = "Agent",
+                        ImagePath = "https://images.hdqwalls.com/download/pennywise-wu-1280x720.jpg"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Samara",
+                        Lastname = "Morgan",
+                        Agency = agencies[7],
+                        Nickname = "The girl from the well",
+                        Email = "samara.morgan@wraithresidences.se",
+                        Username = "samara.morgan@wraithresidences.se",
+                        Password = "Samara@1",
+                        Phonenumber = "0736667788",
+                        Role = "Admin",
+                        ImagePath = "https://th.bing.com/th/id/OIP.MSFxEbiTcS0Hb3cyM-IPKgHaFJ?rs=1&pid=ImgDetMain"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Kayako",
+                        Lastname = "Saeki",
+                        Agency = agencies[7],
+                        Nickname = "The housewife",
+                        Email = "kayako.saeki@wraithresidences.se",
+                        Username = "kayako.saeki@wraithresidences.se",
+                        Password = "Kayako@1",
+                        Phonenumber = "0736667799",
+                        Role = "Agent",
+                        ImagePath = "https://th.bing.com/th/id/R.e0f1e4620f2529a13bc063ace9440b2e?rik=1I%2fNGPPbveP42A&riu=http%3a%2f%2fimages1.fanpop.com%2fimages%2fphotos%2f1900000%2fkayako-saeki-the-grudge-1979258-270-270.jpg&ehk=g6Wy2cvCU3SlI4MJ%2b11d2KbxZHMj5hdT3v7OfZlzYTc%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Jack",
+                        Lastname = "Torrance",
+                        Agency = agencies[6],
+                        Nickname = "Johnny",
+                        Email = "jack.torrance@spookyestates.se",
+                        Username = "jack.torrance@spookyestates.se",
+                        Password = "Torrance@1",
+                        Phonenumber = "0737778899",
+                        Role = "Admin",
+                        ImagePath = "https://www.slashfilm.com/img/gallery/finding-the-right-voice-for-jack-torrance-was-key-to-the-shinings-success/l-intro-1671756580.jpg"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Lydia",
+                        Lastname = "Deetz",
+                        Agency = agencies[6],
+                        Nickname = "The goth girl",
+                        Email = "lydia.deetz@spookyestates.se",
+                        Username = "lydia.deetz@spookyestates.se",
+                        Password = "Deetz@1",
+                        Phonenumber = "0737778898",
+                        Role = "Agent",
+                        ImagePath = "https://th.bing.com/th/id/OIP.UZB4XB4zDO2IkrFJqo_pQgHaHH?rs=1&pid=ImgDetMain"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Kit",
+                        Lastname = "Walker",
+                        Agency = agencies[5],
+                        Nickname = "Fantomen",
+                        Email = "kit.walker@phantomrealty.se",
+                        Username = "kit.walker@phantomrealty.se",
+                        Password = "Walker@1",
+                        Phonenumber = "0738889900",
+                        Role = "Admin",
+                        ImagePath = "https://th.bing.com/th/id/R.8d8d99c510eafa92521caef327ce1ce0?rik=EMpyfGTL5QkpCw&riu=http%3a%2f%2f2.bp.blogspot.com%2f_Y8m29ZLX5ag%2fTCGbt44T2EI%2fAAAAAAAAFoc%2fk8jkrmclJkg%2fs400%2fPHANTOM%2bART%2bFRAME.jpg&ehk=cPmvJ6Jay0INq9mAj8kg2Kj2AaC7O2WK4mMGj0zB%2fVQ%3d&risl=&pid=ImgRaw&r=0"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Christine",
+                        Lastname = "Daaé",
+                        Agency = agencies[5],
+                        Nickname = "The Swedish Nightingale",
+                        Email = "christine.daae@phantomrealty.se",
+                        Username = "christine.daae@phantomrealty.se",
+                        Password = "Daaé@1",
+                        Phonenumber = "0738889901",
+                        Role = "Agent",
+                        ImagePath = "https://vignette4.wikia.nocookie.net/poto/images/e/e8/Christine-daae-and-christines-lace-chemise-gallery.jpg/revision/latest?cb=20111119221243"
+                    },
+                    new AgentDTO
+                    {
+                        Firstname = "Marcus",
+                        Lastname = "Friberg",
+                        Agency = agencies[10],
+                        Nickname = "The Professor",
+                        Email = "marcus.friberg@happy.se",
+                        Username = "marcus.friberg@happy.se",
+                        Password = "Friberg@1",
+                        Phonenumber = "073165789",
+                        Role = "Admin",
+                        ImagePath = "https://filerepository.itslearning.com/e7aec601-7d83-4d49-901e-38b7cdeed767"
                     }
                 };
 
