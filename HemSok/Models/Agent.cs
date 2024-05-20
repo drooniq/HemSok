793998d3893b33650b66eddb22b78d9b8c1e4f6d
Drooniq <emil.waara@protonmail.com>
2024-05-20 12:34:19 +0200

    Added functionality to add user, also set a roll to a user when register.

--- HemSok/Models/Agent.cs
+++ HemSok/Models/Agent.cs
@@ -7,14 +7,23 @@ Author: Marcus Karlsson, Fredrik Blixt, Emil Waara
 */
 namespace HemSok.Models
 {
+
     public class Agent : IdentityUser
     {
+        private const string DefaultAgentImagePath = "images/ghost1.png";
+
         [Required]
         public string FirstName { get; set; } = String.Empty;
         [Required]
         public string LastName { get; set; } = String.Empty;
         public string? Nickname { get; set; }
-        public string? ImagePath { get; set; }
+
+        private string? imagePath = DefaultAgentImagePath;
+        public string? ImagePath
+        {
+            get => string.IsNullOrEmpty(imagePath) ? DefaultAgentImagePath : imagePath;
+            set => imagePath = value;
+        }
         public Agency? Agency { get; set; }
         public Agent() { }
     }
